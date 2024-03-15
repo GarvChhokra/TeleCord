f49e39d6bac069c0e506915ba76db1e36ffad61c
Ali-Arani <94089011+Ali-Arani@users.noreply.github.com>
2024-03-15 13:41:17 -0400

    Revert "ali_1_up"

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './Header'
-import MainContent from './MainContent'
 
 const MainPage = () => {
   return (
     <>
       <Header />
-      <MainContent/>
     </>
   )
 }
